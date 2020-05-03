2821e2e125d492d348f08b47fbe951a231b5a74d
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-03 10:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var empty = [];
-  for(var i=0; i<=musicians.length; i++){
+  for(var i=0; i<musicians.length; i++){
     empty.push(`${musicians[i]} plays ${instruments[i]}`);
     }
  return empty
